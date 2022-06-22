f90a14af586ccfa40c35773f32b2f8e7da437fc5
Johannes Acedo <74800149+Johannesacedo@users.noreply.github.com>
2022-06-22 14:19:57 +0800

    Next-generation_Javascript_and_Typescript

---
+++ let_and_const.js
@@ -0,0 +1,8 @@
+"use strict";
+const Username = 'Hans';
+let age = 30;
+if (age == 30) {
+    let result = 40;
+    console.log(result);
+}
+console.log(Username);
---
+++ let_and_const.ts
@@ -0,0 +1,11 @@
+const Username='Hans';
+
+let age=30;
+
+if(age == 30){
+    let result = 40;
+    console.log(result);
+    
+}
+
+console.log(Username);
